ae839d32e295fbb7d56e0b153c71db8af0a0d084
Administrator <18573300416@163.com>
2019-08-09 14:08:13 +0800

    first commit

---
+++ src/main/java/com/dimple/framework/shiro/service/SwaggerConfig.java
@@ -0,0 +1,6 @@
+package com.dimple.framework.shiro.service;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+}
